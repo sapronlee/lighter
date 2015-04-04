e889583a58b666c8e2e6d21011609bf6cf642099
sapronlee <sapronlee@gmail.com>
2015-04-04 20:21:32 +0800

    封装Config

--- config/application.go
+++ config/application.go
@@ -29,3 +29,28 @@ func ClassicApp() *App {
 func (a *App) Run() {
 	a.Martini.Run()
 }
+
+func (a *App) GetBoolConfig(name string) bool {
+	b, _ := a.Config.Bool(a.Env, name)
+	return b
+}
+
+func (a *App) GetStringConfig(name string) string {
+	s, _ := a.Config.String(a.Env, name)
+	return s
+}
+
+func (a *App) GetIntConfig(name string) int {
+	i, _ := a.Config.Int(a.Env, name)
+	return i
+}
+
+func (a *App) GetFloatConfig(name string) float64 {
+	f, _ := a.Config.Float(a.Env, name)
+	return f
+}
+
+func (a *App) GetRawStringConfig(name string) string {
+	s, _ := a.Config.RawString(a.Env, name)
+	return s
+}
--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"lighter/config"
 )
 
 func main() {
 	app := config.ClassicApp()
-	debug, _ := app.Config.Bool(app.Env, "debug")
-	fmt.Printf("debug is %v", debug)
 	app.Run()
 }
